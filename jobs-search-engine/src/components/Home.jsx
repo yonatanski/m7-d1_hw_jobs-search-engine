2b8c011526d87273d4b86240a83c1675a5c7e6bb
yonatanski <phpcodeyg@gmail.com>
2022-03-01 08:51:26 +0100

    able to fecth from Search box

--- jobs-search-engine/src/components/Home.jsx
+++ jobs-search-engine/src/components/Home.jsx
@@ -1,18 +1,39 @@
 import React from "react"
 import { useEffect, useState } from "react"
-import { Form, Button, FormControl } from "react-bootstrap"
+import { Form, Button, FormControl, Container, Row, Col } from "react-bootstrap"
 import JobResults from "./JobResults"
 
 export default function Home() {
   const [job, setJob] = useState([])
+  const [query, setQuery] = useState("")
 
-  useEffect(() => {
-    jobFetch()
-  }, [])
+  //   useEffect(() => {
+  //     jobFetch()
+  //   }, [])
 
-  const jobFetch = async () => {
+  //   const jobFetch = async () => {
+  //   try {
+  //     let response = await fetch(`https://strive-jobs-api.herokuapp.com/jobs?search=developer&limit=10`)
+
+  //     if (response.ok) {
+  //       let jobdata = await response.json()
+  //       console.log("jsoneddd", jobdata.data)
+
+  //       setJob(jobdata.data)
+  //     }
+  //     console.log("state after setjobed", job)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  const handelChange = (e) => {
+    setQuery(e.target.value)
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
     try {
-      let response = await fetch(`https://strive-jobs-api.herokuapp.com/jobs?search=developer&limit=10`)
+      let response = await fetch(`https://strive-jobs-api.herokuapp.com/jobs?search=${query}&limit=10`)
+      console.log(response)
 
       if (response.ok) {
         let jobdata = await response.json()
@@ -26,19 +47,28 @@ export default function Home() {
     }
   }
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6 mt-3">
+    <Container>
+      <Row>
+        <Col xs={12}>
           <h3>STRIVE JOB SEARCHING PLATFORM</h3>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
+          <Form onSubmit={handleSubmit}>
+            <FormControl type="text" placeholder="Search" className=" mr-sm-2" value={query} onChange={handelChange} />
             <Button className="mt-3" type="submit">
               Search
             </Button>
           </Form>
-          <JobResults fecthedJobs={job} />
-        </div>
-      </div>
-    </div>
+          {/* <JobResults fecthedJobs={job} /> */}
+        </Col>
+        <Col>
+          {job.map((job) => {
+            return (
+              <div>
+                {job.company_name}and {job.title}
+              </div>
+            )
+          })}
+        </Col>
+      </Row>
+    </Container>
   )
 }
--- jobs-search-engine/src/components/JobResults.jsx
+++ jobs-search-engine/src/components/JobResults.jsx
@@ -1,16 +1,20 @@
-import React from "react"
-// import { useEffect, useState } from "react"
-// import { Form, Button, FormControl } from "react-bootstrap"
-import { ListGroup } from "react-bootstrap"
+// import React from "react"
+// // import { useEffect, useState } from "react"
+// // import { Form, Button, FormControl } from "react-bootstrap"
+// import { ListGroup } from "react-bootstrap"
 
-export default function JobResults(props) {
-  return (
-    <div>
-      {props.data.map((job) => (
-        <ListGroup>
-          <ListGroup.Item>{job.title}</ListGroup.Item>
-        </ListGroup>
-      ))}
-    </div>
-  )
-}
+// export default function JobResults({ job }) {
+//   return (
+//     <div>
+//       <h1>hellooooooooooooooooo</h1>
+//       {job ||
+//          <ListGroup>
+//         [].map((job) => (
+
+//             <ListGroup.Item>{job.title} hello</ListGroup.Item>
+
+//         ))}
+//         </ListGroup>
+//     </div>
+//   )
+// }
